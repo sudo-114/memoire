e376931f74c40585b0e218de4271eb6975a7dfef
sudo-114 <c7306870@gmail.com>
2025-11-22 02:11:15 +0000

    theme with darkmode | ShadcnUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main>{children}</main>
+
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
